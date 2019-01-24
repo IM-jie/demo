ff4204aecce79c9f09b5e1a1107e03b672f7e29a
huangj <sx-huangj9548@dtdream.com>
2019-01-24 15:48:46 +0800

    添加streamwiter

--- service/src/main/java/com/jie/service/serviceimpl/TestServiceImpl.java
+++ service/src/main/java/com/jie/service/serviceimpl/TestServiceImpl.java
@@ -1,5 +1,6 @@
 package com.jie.service.serviceimpl;
 
+import com.alibaba.datax.core.Engine;
 import com.jie.service.iservice.ITestService;
 import org.springframework.stereotype.Service;
 
@@ -7,7 +8,25 @@ import org.springframework.stereotype.Service;
 public class TestServiceImpl implements ITestService {
     @Override
     public String test() {
+        System.setProperty("datax.home", getCurrentClasspath());
+        String[] datxArgs = {"-job", getCurrentClasspath() + "/job/stream2stream.json", "-mode", "standalone", "-jobid", "-1"};
+        try {
+            Engine.entry(datxArgs);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        return System.getProperty("datax.home");
+    }
 
-        return "test";
+    public static String getCurrentClasspath() {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        String currentClasspath = classLoader.getResource("").getPath();
+        // 当前操作系统
+        String osName = System.getProperty("os.name");
+        if (osName.startsWith("Windows")) {
+            // 删除path中最前面的/
+            currentClasspath = currentClasspath.substring(1);
+        }
+        return currentClasspath;
     }
 }
